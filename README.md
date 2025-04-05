a72653bd492c233e13a674bc9a7d4840c8a79567
Alexander Eremenchuk <alexer92@yandex.ru>
2025-04-05 06:50:27 +0300

    feat: create window using SDL2

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # tetris
+
 Tetris written in Rust
+
+## Install prerequisites
+
+Before building the project, you need to install the SDL2 library.
+
+### Linux
+
+```bash
+sudo apt install libsdl2-dev
+```
+
+## Build and run
+
+1. Clone this repository:
+
+```bash
+git clone https://github.com/AlexEr92/tetris
+```
+
+2. Build and run with `cargo`:
+
+```bash
+cargo run --release
+```
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use sdl2::{event::Event, keyboard::Keycode, pixels::Color};
+use std::thread::sleep;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello, world!");
+    let sdl_context = sdl2::init().expect("SDL initialization failed");
+    let video_subsystem = sdl_context
+        .video()
+        .expect("Couldn't get SDL video subsystem");
+
+    let window = video_subsystem
+        .window("sdl2 demo", 800, 600)
+        .position_centered()
+        .opengl()
+        .build()
+        .expect("Failed to create window");
+
+    let mut canvas = window
+        .into_canvas()
+        .build()
+        .expect("Failed to convert window into canvas");
+
+    canvas.set_draw_color(Color::RGB(255, 0, 0));
+    canvas.clear();
+    canvas.present();
+
+    let mut running = true;
+    let mut event_pump = sdl_context
+        .event_pump()
+        .expect("Failed to get SDL event pump");
+
+    while running {
+        for event in event_pump.poll_iter() {
+            match event {
+                Event::Quit { .. }
+                | Event::KeyDown {
+                    keycode: Some(Keycode::Escape),
+                    ..
+                } => {
+                    running = false;
+                }
+                _ => {}
+            }
+        }
+        sleep(Duration::new(0, 1_000_000_000u32 / 60));
+    }
 }
